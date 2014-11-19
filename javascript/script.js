45a26921befb27a3e886051e89576dba150415b9
Sopheary Chiv <hsopheary@yahoo.com>
2014-11-19 15:43:16 -0800

    initialize repo

---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+//Function called "signIn", causes an alert to pop up with text "Thanks for signing in". Should be triggered by clicking on the button with id of "sign-in".
+
+function signIn () {
+    var signIn = "Thanks for signing in";
+    alert(signIn);
+    document.getElementById("sign-in").value= signIn;
+}
+
+
+//Function called "signUp", causes a prompt to appear and ask for user's email address, then prints to the "output-here" paragraph "Thanks for signing up, " and the entered email address. Should be triggered by clicking on the button with id of "sign-up".
+
+function signUp(){
+    var email = prompt("Please enter your email address");
+    document.getElementById("output-here").innerHTML = "Thanks for signing up, " + email;    
+}
+
+function thirdButton(){
+    var extra = alert(" Just a third button! :)");
+}
